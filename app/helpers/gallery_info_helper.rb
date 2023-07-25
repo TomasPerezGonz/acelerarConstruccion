db64e6e85583e308abe3aba7372905f2c49a2cfa
Tomas <tomasperezg@gmail.com>
2023-07-24 21:57:25 -0400

    Se agrega controlador about

---
+++ app/helpers/gallery_info_helper.rb
@@ -0,0 +1,2 @@
+module GalleryInfoHelper
+end
---
+++ test/controllers/gallery_info_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class GalleryInfoControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
